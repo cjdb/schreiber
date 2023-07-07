c952470db79dc10386e46adcd26f75b07b40b098
Christopher Di Bella <cjdb.ns@gmail.com>
2023-07-06 21:02:02 -0700

    adds `classof` to AST components
    Since we're not using RTTI for this project, we need to leverage LLVM's
    alternative to `dynamic_cast`. This is achieved by getting the class to
    identify itself to `llvm::dyn_cast`, which then performs a conversion on
    our behalf.

--- source/info.cpp
+++ source/info.cpp
@@ -14,11 +14,13 @@
 
 namespace info {
 	decl_info::decl_info(
+	  kind kind,
 	  clang::Decl const* decl,
 	  std::string description,
 	  std::vector<header_info> headers,
 	  std::vector<module_info> modules) noexcept
-	: decl_(decl)
+	: kind_(kind)
+	, decl_((CJDB_ASSERT(decl != nullptr), decl))
 	, description_(std::move(description))
 	, headers_(std::move(headers))
 	, modules_(std::move(modules))
@@ -55,7 +57,7 @@ namespace info {
 	  std::vector<exits_via_info> exits_via,
 	  std::vector<header_info> headers,
 	  std::vector<module_info> modules) noexcept
-	: decl_info(decl, std::move(description), std::move(headers), std::move(modules))
+	: decl_info(kind::function_info, decl, std::move(description), std::move(headers), std::move(modules))
 	, parameters_((CJDB_EXPECTS(parameters.size() <= decl->param_size()), std::move(parameters)))
 	, returns_(std::move(returns))
 	, preconditions_(std::move(preconditions))
@@ -77,7 +79,7 @@ namespace info {
 	  std::vector<exits_via_info> exits_via,
 	  std::vector<header_info> headers,
 	  std::vector<module_info> modules) noexcept
-	: decl_info(decl, std::move(description), std::move(headers), std::move(modules))
+	: decl_info(kind::function_info, decl, std::move(description), std::move(headers), std::move(modules))
 	, template_parameters_(std::move(template_parameters))
 	, parameters_(std::move(parameters))
 	, exception_specifier_(std::move(exception_specifier))
@@ -128,25 +130,40 @@ namespace info {
 		return exits_via_;
 	}
 
+	auto function_info::classof(decl_info const* const decl) -> bool
+	{
+		return get_kind(*decl) == kind::function_info;
+	}
+
 	parameter_info::parameter_info(clang::ParmVarDecl const* decl, std::string description) noexcept
-	: decl_info(decl, std::move(description))
+	: decl_info(kind::parameter_info, decl, std::move(description))
 	{}
+
+	auto parameter_info::classof(decl_info const* const decl) -> bool
+	{
+		return get_kind(*decl) == kind::parameter_info;
+	}
 
 	template_parameter_info::template_parameter_info(
 	  clang::TemplateTypeParmDecl const* decl,
 	  std::string description)
-	: decl_info(decl, std::move(description))
+	: decl_info(kind::template_parameter_info, decl, std::move(description))
 	{}
 
 	template_parameter_info::template_parameter_info(
 	  clang::NonTypeTemplateParmDecl const* decl,
 	  std::string description)
-	: decl_info(decl, std::move(description))
+	: decl_info(kind::template_parameter_info, decl, std::move(description))
 	{}
 
 	template_parameter_info::template_parameter_info(
 	  clang::TemplateTemplateParmDecl const* decl,
 	  std::string description)
-	: decl_info(decl, std::move(description))
+	: decl_info(kind::template_parameter_info, decl, std::move(description))
 	{}
+
+	auto template_parameter_info::classof(decl_info const* const decl) -> bool
+	{
+		return get_kind(*decl) == kind::template_parameter_info;
+	}
 } // namespace info
