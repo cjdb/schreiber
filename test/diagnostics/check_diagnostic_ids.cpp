9ef0330ec3b82169407de77aa54481dd74bc7334
Christopher Di Bella <cjdb.ns@gmail.com>
2023-07-09 00:01:16 -0700

    installs TableGen and uses it to generate diagnostics
    TableGen is an effective way to organise and generate things such as
    diagnostic data and recognised symbols. It's a little bit Clangey at
    the moment, but that can probably be smoothed out over time, but this
    is to be expected since the structure and a lot of the code is directly
    imported from Clang.

---
+++ test/diagnostics/check_diagnostic_ids.cpp
@@ -0,0 +1,67 @@
+// Copyright (c) Google LLC.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+#include <catch2/catch_test_macros.hpp>
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/Decl.h>
+#include <clang/AST/DeclCXX.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/Basic/Diagnostic.h>
+#include <clang/Frontend/ASTUnit.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/raw_ostream.h>
+#include <schreiber/diagnostic_ids.hpp>
+#include <string>
+
+namespace {
+	namespace ast_matchers = clang::ast_matchers;
+	namespace tooling = clang::tooling;
+
+	using ast_matchers::functionDecl;
+	using ast_matchers::match;
+	using ast_matchers::selectFirst;
+
+	TEST_CASE("errors can be emitted")
+	{
+		auto text = std::string();
+		auto stream = llvm::raw_string_ostream(text);
+
+		auto ast = tooling::buildASTFromCode("int f();");
+		auto& context = ast->getASTContext();
+		auto& engine = context.getDiagnostics();
+		engine.setClient(new clang::TextDiagnosticPrinter(stream, &engine.getDiagnosticOptions(), false));
+		diag::add_diagnostics(engine);
+
+		auto const* decl =
+		  selectFirst<clang::FunctionDecl>("decl", match(functionDecl().bind("decl"), context));
+
+		REQUIRE(decl != nullptr);
+		REQUIRE(engine.getNumErrors() == 0);
+		REQUIRE(engine.getNumWarnings() == 0);
+
+		SECTION("reports an error")
+		{
+			engine.Report(diag::err_unknown_directive) << "\\raven";
+			CHECK(text == "error: unknown directive `\\raven`\n");
+			CHECK(engine.getNumErrors() == 1);
+			CHECK(engine.getNumWarnings() == 0);
+		}
+
+		SECTION("reports a warning")
+		{
+			engine.Report(diag::warn_undocumented_decl) << decl->getCanonicalDecl();
+			CHECK(engine.getNumErrors() == 0);
+			CHECK(engine.getNumWarnings() == 1);
+
+			engine.Report(diag::note_undocumented_decl) << decl;
+			CHECK(text
+			      == "warning: function 'f' is not documented\n"
+			         "note: use `\\undocumented` to indicate that 'f' should not be documented\n");
+
+			CHECK(engine.getNumErrors() == 0);
+			CHECK(engine.getNumWarnings() == 1);
+		}
+	}
+} // namespace
